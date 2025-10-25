a313f623bbe478c981b40c87bca7e5d419490fcb
Ayoub <ayoub.anam69@gmail.com>
2025-10-25 15:06:46 +0100

    Added do_while example

---
+++ lesson 4/do_while_loop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int x = 0;
+
+    do{
+
+        cout<<"x + 1 = ";
+        x = x + 1;
+        cout<<x<<endl;
+
+    }
+    while(x < 10);
+}
