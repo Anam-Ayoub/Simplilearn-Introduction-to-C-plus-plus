165c80e09cffd183b3f11c4a0f63a6aa6d0f0396
Ayoub <ayoub.anam69@gmail.com>
2025-10-25 16:05:13 +0100

    Added nested_loop example

---
+++ lesson 4/nested_loop.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int x;
+    int y;
+
+    for (x=0 ; x<=10 ; x++){
+        
+        for ( y=0 ; y<=10 ; y++){
+          cout<<"x = "<<x<<" y = "<<y<<endl;
+          //cout<<x<<" "<<y<<endl;  
+        }
+        cout<<"------------\n";
+    }
+
+    return 0;
+
+}
