d7a5525ad5ad26a0e3f280b3aa50c85dfe420c97
Ayoub <ayoub.anam69@gmail.com>
2025-10-26 15:36:28 +0100

    Added star_pyramid_example.cpp

---
+++ lesson 4/star_pyramid_example.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+
+    cout<<"How many lines you wish for? --> ";
+    cin>>n;
+
+    for (int i = 1 ; i <= n*2 ; i+=2){
+        for (int k = n-1 ; k>=(i/2)+1 ; k--){
+            cout<<" ";
+        }
+        for (int j = 1 ; j <= i ; j++){
+
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+}
