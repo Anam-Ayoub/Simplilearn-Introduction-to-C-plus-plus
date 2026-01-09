20267ca6b8500326f400f7dd24579eb78f2c99d9
ANAM <ayoub.anam69@gmail.com>
2026-01-09 17:39:14 +0100

    Added pointers example 1

--- lesson 8/pointers_ex_1.cpp
+++ lesson 8/pointers_ex_1.cpp
@@ -1,8 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
 
-    
+    int x;
+    int *p;
 
+    x = 70;
+    p = &x;
+
+    cout<<"x addres : "<<p<<endl;
+    cout<<"Value referencing p is : "<<*p;
+    return 0;
 }
---
+++ lesson 8/pointers_ex_2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n1, n2;
+    int sum = 0;
+    int sub = 0;
+    int *ptr1;
+    int *ptr2;
+    
+    cout<<"Dakhel dakhel --> ";
+    cin>>n1;
+    cout<<"3awd dakhel zid dakhel--> ";
+    cin>>n2;
+    ptr1 = &n1;
+    ptr2 = &n2;
+    (*ptr1)++;
+    (*ptr2)++;
+    cout<<"Hahoma tzado bwa7d:"<<endl;
+    cout<<n1<<endl;
+    cout<<n2<<endl;
+    return 0;
+}
