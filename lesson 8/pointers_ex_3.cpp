023f8cf81cc6d7376cfa207e103a4778722d56f8
ANAM <ayoub.anam69@gmail.com>
2026-01-09 18:09:38 +0100

    Added pointers example 3

---
+++ lesson 8/pointers_ex_3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int arr[] = {1,2,3,4,5,6,7,9};
+    int i;
+    for (i = 0 ; i <= 8 ; i++){
+        cout<<*arr + i<<endl;
+    }
+    return 0;
+}
